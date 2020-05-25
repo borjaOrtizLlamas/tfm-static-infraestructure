05a118aeb319ed2a18f4fd75fb865bf746ff3b0f
borja <borja.ortizllamas1@gmail.com>
2020-05-26 01:19:33 +0200

    adding  small_comerce_api_rest

--- ECR.tf
+++ ECR.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "foo" {
-  name                 = "crud_cliend_with_products"
+  name  = "small_comerce_api_rest"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = false
