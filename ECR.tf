d98e195957e7ba96b1655ee6da47204103a2a480
borja <borja.ortizllamas1@gmail.com>
2020-06-04 22:36:52 +0200

    cambio ecr

--- ECR.tf
+++ ECR.tf
@@ -5,3 +5,12 @@ resource "aws_ecr_repository" "foo" {
     scan_on_push = false
   }
 }
+
+
+resource "aws_ecr_repository" "small_comerce_api_rest_ecr_repository" {
+  name  = "small_comerce_api_rest"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
