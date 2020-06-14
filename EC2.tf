602f5025d4e4f32b7ccc5972e2f5ce71d8319949
borja <borja.ortizllamas1@gmail.com>
2020-06-14 04:49:50 +0200

    ecr

--- EC2.tf
+++ EC2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jenkins" {
   ami = "ami-088d9b82e3a8b4f7a" #This ami is generated with packer, the code is in this repository: https://github.com/borjaOrtizLlamas/jenkinsAMI.git
-  instance_type = "t2.micro"
+  instance_type = "t2.xlarge"
   depends_on = ["aws_internet_gateway.openWorld"]
   availability_zone = "${var.ZONE_SUB}"
   key_name = "${aws_key_pair.jenkinsSSH.key_name}"
--- ECR.tf
+++ ECR.tf
@@ -1,4 +1,4 @@
-resource "aws_ecr_repository" "foo" {
+resource "aws_ecr_repository" "small_comerce_api_rest" {
   name  = "small_comerce_api_rest"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
