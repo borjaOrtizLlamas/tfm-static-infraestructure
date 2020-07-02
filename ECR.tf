297e0c427264484651d18f303e6c10414dbd84b7
borja <borja.ortizllamas1@gmail.com>
2020-07-02 18:07:21 +0200

    jenkins ami and ecr

--- ECR.tf
+++ ECR.tf
@@ -6,17 +6,8 @@ resource "aws_ecr_repository" "small_comerce_api_rest" {
   }
 }
 
-
 resource "aws_ecr_repository" "sidecar" {
   name  = "beats_sidecar"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-resource "aws_ecr_repository" "test" {
-  name  = "test"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = false
