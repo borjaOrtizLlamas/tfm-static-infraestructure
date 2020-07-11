75079b0c7b54c85d99206cd8556031eba41c1699
borja <borja.ortizllamas1@gmail.com>
2020-07-11 14:49:49 +0200

    file beat

--- ECR.tf
+++ ECR.tf
@@ -7,7 +7,7 @@ resource "aws_ecr_repository" "small_comerce_api_rest" {
 }
 
 resource "aws_ecr_repository" "sidecar" {
-  name  = "beats_sidecar"
+  name  = "filebeats_unir"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = false
