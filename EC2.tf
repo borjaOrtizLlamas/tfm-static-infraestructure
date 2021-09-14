b273dee9cbc4621c2f9bff5d23e0e3add444843e
unknown <ortizllamasborja@gmail.com>
2021-09-14 16:44:48 +0200

    adding changes

--- EC2.tf
+++ EC2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jenkins" {
-  ami = "ami-093620e0e4f2f5f0f" #This ami is generated with packer, the code is in this repository: https://github.com/borjaOrtizLlamas/jenkinsAMI.git
-  instance_type = "t2.xlarge"
+  ami = "ami-0dea8ccae78a56f9c" #This ami is generated with packer, the code is in this repository: https://github.com/borjaOrtizLlamas/jenkinsAMI.git
+  instance_type = "t2.small"
   depends_on = ["aws_internet_gateway.openWorld"]
   availability_zone = "${var.ZONE_SUB}"
   key_name = "${aws_key_pair.jenkinsSSH.key_name}"
@@ -8,9 +8,11 @@ resource "aws_instance" "jenkins" {
     network_interface_id = "${aws_network_interface.jenkins_interface.id}"
     device_index = 0
   }
+
   root_block_device{
-    volume_size = 500
+    volume_size = 8
   }
+  
   tags = {
     Name = "JENKINS"
     Environment = "PRO"
--- ECR.tf
+++ ECR.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "small_comerce_api_rest" {
   name  = "small_comerce_api_rest"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = false
   }
@@ -8,7 +8,7 @@ resource "aws_ecr_repository" "small_comerce_api_rest" {
 
 resource "aws_ecr_repository" "sidecar" {
   name  = "filebeats_unir"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = false
   }
