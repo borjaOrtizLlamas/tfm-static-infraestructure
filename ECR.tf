de979acc9877ef975642aa316ba6a7f6e0360d01
unknown <ortizllamasborja@gmail.com>
2021-09-14 17:35:13 +0200

    deleting ecr repository sidecar

--- ECR.tf
+++ ECR.tf
@@ -6,10 +6,3 @@ resource "aws_ecr_repository" "small_comerce_api_rest" {
   }
 }
 
-resource "aws_ecr_repository" "sidecar" {
-  name  = "filebeats_unir"
-  image_tag_mutability = "IMMUTABLE"
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
